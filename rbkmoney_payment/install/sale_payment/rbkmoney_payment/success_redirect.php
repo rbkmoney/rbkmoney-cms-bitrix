d35d1191ae1ceb0c58da9ffc52e4bb84d95a4792
avcherkasov <shnurok14@ya.ru>
2017-03-23 13:54:02 +0300

    ITS-23: changed url for redirection

--- rbkmoney_payment/install/sale_payment/rbkmoney_payment/success_redirect.php
+++ rbkmoney_payment/install/sale_payment/rbkmoney_payment/success_redirect.php
@@ -1,6 +1,4 @@
 <?php
-if ($orderID = !empty($_REQUEST['orderId']) ? $_REQUEST['orderId'] : '') {
-    header('Location: /personal/order/rbkmoney_payment/success.php?orderId=' . $orderID, true, 301);
-    exit();
-}
+header('Location: /personal/order/rbkmoney_payment/success.php', true, 301);
+exit();
 ?>
